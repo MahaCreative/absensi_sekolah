3ee742dc02c724170e1f1d346db01095cf3f5366
MahaCreative <gunturmadjid.3@gmail.com>
2025-08-20 01:35:11 +0800

    update

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Numpang\MonAIrController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('get-mon-air', [MonAIrController::class, 'index']);
+Route::post('store-mon-air', [MonAIrController::class, 'store']);
